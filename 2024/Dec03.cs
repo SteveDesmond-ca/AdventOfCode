b218ffb96c50658179e3f4e6e47795d873bf02ec
Steve Desmond <Steve@ecoAPM.com>
2024-12-03 09:29:39 -0500

    Add solution for Dec03

---
+++ 2024/Dec03.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+internal sealed class Dec03 : Puzzle
+{
+    private static readonly Regex Matcher = RegexHelpers.Dec03();
+
+    public override int Part1()
+        => GetMultipliedSum(Data);
+
+    private static int GetMultipliedSum(string input)
+        => Matcher.Matches(input).Sum(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+
+    public override int Part2()
+        => Data.Split("do()").Select(s => s.Split("don't()")[0]).Sum(GetMultipliedSum);
+}
---
+++ 2024/RegexHelpers.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+
+internal static partial class RegexHelpers
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    public static partial Regex Dec03();
+}
