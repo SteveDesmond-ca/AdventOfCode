1f24fb098bbd7fc859ca4f2c37f2a06401931206
Steve Desmond <Steve@ecoAPM.com>
2024-12-02 22:41:37 -0500

    Add solution for Dec02

---
+++ 2024/Dec02.cs
@@ -0,0 +1,54 @@
+﻿internal sealed class Dec02 : Puzzle
+{
+    private enum Direction
+    {
+        Up,
+        Down
+    }
+
+    public override int Part1()
+    {
+        var reports = Data.Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+        return reports.Count(IsSafe);
+    }
+
+    private static bool IsSafe(string report)
+    {
+        var levels = report.Split(" ").Select(byte.Parse).ToArray();
+        var direction = levels[0] < levels[1] ? Direction.Up : Direction.Down;
+
+        for (var x = 1; x < levels.Length; x++)
+        {
+            if (direction == Direction.Up && levels[x - 1] > levels[x]
+                || direction == Direction.Down && levels[x - 1] < levels[x]
+                || Math.Abs(levels[x-1] - levels[x]) > 3
+                || Math.Abs(levels[x-1] - levels[x]) < 1)
+                return false;
+        }
+
+        return true;
+    }
+
+    public override int Part2()
+    {
+        var reports = Data.Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+        return reports.Count(IsSafeWhenDampened);
+    }
+
+    private static bool IsSafeWhenDampened(string report)
+    {
+        if (IsSafe(report))
+            return true;
+        
+        var levels = report.Split(" ").Select(byte.Parse).ToArray();
+        for (var x = 0; x < levels.Length; x++)
+        {
+            var dampened = levels.ToList();
+            dampened.RemoveAt(x);
+            if (IsSafe(string.Join(" ", dampened)))
+                return true;
+        }
+
+        return false;
+    }
+}
