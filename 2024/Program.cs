e8301a4f4abfa25861b204b1b4c654247fe22ef2
Steve Desmond <Steve@ecoAPM.com>
2024-12-05 09:53:35 -0500

    Show execution times in console output

--- 2024/Program.cs
+++ 2024/Program.cs
@@ -1,10 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
+
+var timer = Stopwatch.StartNew();
 
 var puzzles = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(Puzzle)))
-    .Select(Activator.CreateInstance).Cast<Puzzle>();
+    .Select(Activator.CreateInstance).Cast<Puzzle>().ToArray();
+
+Console.WriteLine($"Found {puzzles.Length} puzzles ({timer.ElapsedMilliseconds}ms)");
 
 foreach (var puzzle in puzzles)
 {
-    Console.WriteLine(puzzle.GetType().Name + " Part 1: " + puzzle.Part1());
-    Console.WriteLine(puzzle.GetType().Name + " Part 2: " +puzzle.Part2());
+    Console.WriteLine();
+    
+    timer.Restart();
+    Console.WriteLine($"{puzzle.GetType().Name} Part 1: {puzzle.Part1()} ({timer.ElapsedMilliseconds}ms)");
+    
+    timer.Restart();
+    Console.WriteLine($"{puzzle.GetType().Name} Part 2: {puzzle.Part2()} ({timer.ElapsedMilliseconds}ms)");
 }
